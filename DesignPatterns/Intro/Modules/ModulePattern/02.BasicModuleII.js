c2592124bc0834cc0eb9aa32d518f447969596a2
HemantSinghEdu <34578863+HemantSinghEdu@users.noreply.github.com>
2018-08-07 15:39:43 +0530

    Update and rename 02.BasicModuleII.js to 02.BasicModule-II-Problem.js

--- DesignPatterns/Intro/Modules/ModulePattern/02.BasicModuleII.js
+++
@@ -1,6 +0,0 @@
-/*
-   The basic Tile module in previous lesson had some drawbacks. It does not represent a proper class.
-   We can't create its objects. It resembles a Static class of Java or C#, more than anything else.
-   
-   
-*/
