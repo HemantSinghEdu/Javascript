ff79bc3796ee370eff8e05886019a5823e2c556f
HemantSinghEdu <34578863+HemantSinghEdu@users.noreply.github.com>
2018-08-07 17:33:27 +0530

    Update 01.ModulePattern.js

--- DesignPatterns/Intro/01.Modules/01.ModulePattern.js
+++ DesignPatterns/Intro/01.Modules/01.ModulePattern.js
@@ -1,33 +1,52 @@
 /*
   A module is a small unit of independent and reusable code. It is similar to a Class in Java and C# (but not exactly same)
-  A module helps us remove our variables and functions from the global scope and keep them encapsulated as a reusable entity (module)
-  Below is a basic Tile module that will append a tile to the html page along with some data. 
+  A module helps us remove our variables and functions from the global scope and keep them encapsulated as a reusable entity
+  The basic structure of a module pattern is this:
+        
+        var module = (function()
+        {
+            return 
+            {
+              msg   : "Hello"
+              echo  : function(){ return this.msg; }
+            }
+        })();
+  
+  Basically, we have an anonymous function which is returning an object, and our module gets the object
+  
+  Usage: We can call the module parameters as shown below
+        console.log( module.echo() );
+        
+   Below is a basic Tile module that will append a tile to the html page along with some data. 
 */
 
-var Tile = 
+var Tile = (function()
 {
-    width   : 100,
-    height  : 100,
-  
-    add  : function(container, id)
+    return 
     {
-        $(container).append("<div id='"+ id +"' height='" + this.height + "' width='" + this.width + "'> </div>");
-    },
-    
-    appendText: function(line, id)
-    {
-        var tile = $('#' + id);
-        $(tile).append(line);
+        width   : 100,
+        height  : 100,
+
+        add  : function(container, id)
+        {
+            $(container).append("<div id='"+ id +"' height='" + this.height + "' width='" + this.width + "'> </div>");
+        },
+
+        appendText: function(line, id)
+        {
+            var tile = $('#' + id);
+            $(tile).append(line);
+        }
     }
-}
+})();
 
 
 $(function(){
-  Tile.add("body", 1);
+  Tile.add("body", 1);                  //append a tile to the page
   Tile.appendText("Line 1", 1);
   Tile.appendText("Line 2", 1);
   
-  Tile.add("body", 2);
+  Tile.add("body", 2);                  //append a second tile to the page
   Tile.appendText("Line A", 2);
   Tile.appendText("Line B", 2);
 });
