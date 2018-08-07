99c1ee53f389328942c8697c179c4e8720e32d86
HemantSinghEdu <34578863+HemantSinghEdu@users.noreply.github.com>
2018-08-07 15:22:55 +0530

    Update and rename 01.BasicModule.js to 01.BasicModule-I.js

---
+++ DesignPatterns/Intro/Modules/ModulePattern/01.BasicModule-I.js
@@ -0,0 +1,32 @@
+/*
+  A module is a small unit of independent and reusable code. It is similar to a Class in Java and C#
+  Below is a basic Tile module that will append a tile to the html page along with some data. 
+*/
+
+var Tile = 
+{
+    width   : 100,
+    height  : 100,
+  
+    add  : function(container, id)
+    {
+        $(container).append("<div id='"+ id +"' height='" + this.height + "' width='" + this.width + "'> </div>");
+    },
+    
+    appendText: function(line, id)
+    {
+        var tile = $('#' + id);
+        $(tile).append(line);
+    }
+}
+
+
+$(function(){
+  Tile.add("body", 1);
+  Tile.appendText("Line 1", 1);
+  Tile.appendText("Line 2", 1);
+  
+  Tile.add("body", 2);
+  Tile.appendText("Line A", 2);
+  Tile.appendText("Line B", 2);
+});
