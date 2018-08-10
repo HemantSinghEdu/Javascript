64401f2790725b5817858276a0d98fc4a3219ebb
HemantSinghEdu <34578863+HemantSinghEdu@users.noreply.github.com>
2018-08-10 15:00:58 +0530

    Update and rename DesignPatterns/01.Intro/02.Class/01.Class_OldSyntax.js to DesignPatterns/01.Intro/02.Class/01.OldSyntax/01.SimpleClass.js

---
+++ DesignPatterns/01.Intro/02.Class/01.OldSyntax/01.SimpleClass.js
@@ -0,0 +1,20 @@
+/*
+    There are no classes in the old javascript syntax.
+    We use functions to simulate class-like behavior
+*/
+
+//simple class
+function Employee() {
+  this.fname = "";
+  this.lname = "";
+  this.getName = function(){
+  	return this.fname+' ' +this.lname;
+  }
+}
+
+//Usage
+emp = new Employee();
+emp.fname = "John";
+emp.lname = "Doe";
+var fullName = emp.getName();
+$("body").append("<div>Simple class: "+fullName+"</div>");
