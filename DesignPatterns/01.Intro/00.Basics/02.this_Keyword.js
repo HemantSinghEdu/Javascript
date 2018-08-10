1ab171ccb54077b0cec266c1962f6989c0266743
HemantSinghEdu <34578863+HemantSinghEdu@users.noreply.github.com>
2018-08-10 17:36:12 +0530

    Update 02.this_Keyword.js

--- DesignPatterns/01.Intro/00.Basics/02.this_Keyword.js
+++ DesignPatterns/01.Intro/00.Basics/02.this_Keyword.js
@@ -1,1 +1,22 @@
+/*
+    The this keyword in javascript points to the current object in which our function is EXECUTED.
+    This current object MAY OR MAY NOT be the object which actually DEFINED our function.
+*/
 
+var X = function () {
+        this.name = 'this name is X';
+        this.print = function () {
+            $('body').append("<div>"+this.name+"</div>");
+        }
+}
+
+var Y = function () {
+        this.name = 'this name is Y';
+}
+
+var x = new X();
+var y = new Y();
+y.print = x.print; // Copy the getName function from X object into Y object
+
+x.print();    // Outputs 'X'
+y.print();    // Outputs 'Y' even though the function is defined inside X
