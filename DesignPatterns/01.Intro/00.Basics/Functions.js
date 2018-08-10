022a162b396707af6dd39f53896cdfcd79c1dc70
HemantSinghEdu <34578863+HemantSinghEdu@users.noreply.github.com>
2018-08-10 17:09:52 +0530

    Update Functions.js

--- DesignPatterns/01.Intro/00.Basics/Functions.js
+++ DesignPatterns/01.Intro/00.Basics/Functions.js
@@ -1,34 +1,32 @@
 /*
     In JavaScript, functions are objects, because they can have properties and methods just like any other object. 
-    What distinguishes them from other objects is that functions can be called. In brief, they are Function objects.
+    What distinguishes them from other objects is that functions can be called and they can return values. 
+    In brief, they are Function objects.
 */
 
 //a function declaration
-function print(msg)
-{
-		$('body').append("<div>"+msg+"</div>");
+function print(msg){
+	$('body').append("<div>"+msg+"</div>");
 }
 
 //function expression (anonymous function assigned to a variable)
 var addNum = function(num1, num2){
-  var sum = num1 + num2;
-  return sum;
+	var sum = num1 + num2;
+	return sum;
 }
 
 //function used as a class
-function Calculator(n1, n2)
-{
-  var num1 = n1;
-  var num2 = n2;
-  
-  var add = function(){
-    return num1 + num2;
-  }
+function Calculator(n1, n2){
+	this.num1 = n1;
+	this.num2 = n2;
+	this.add = function(){
+	  return this.num1 + this.num2;
+	}
 }
 
 $(function(){
     print("addNum : " + addNum(1,2));
     
-    var calc = new Calculator(5,6);       //creating an object
+    var calc = new Calculator(5,6);       //creating an object of our class
     print("calculator: " + calc.add());       
 });
